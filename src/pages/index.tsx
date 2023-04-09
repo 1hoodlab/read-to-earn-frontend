6bb9ba90f6efc03a6fac2376aa01ddcd8363ade3
Vuong Q. Huy <119883477+vnghuygithub@users.noreply.github.com>
2023-04-09 22:10:51 +0700

    Commit to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import styles from "@/styles/Home.module.scss";
 import Header from "@/components/header";
-import FstA from "@/components/fstA";
+import FstArticle from "@/components/fstArticle";
+import Menu from "@/components/menu";
 import { NextSeo } from "next-seo";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Header />
-      <FstA />
+      <Menu />
+      <FstArticle />
     </main>
   );
 }
