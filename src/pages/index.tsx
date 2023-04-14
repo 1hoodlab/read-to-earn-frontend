a187c1dfb143b7c35d712007c095a2a6c1392891
Vuong Q. Huy <119883477+vnghuygithub@users.noreply.github.com>
2023-04-14 17:08:23 +0700

    commit to Profile_Page_For_Writer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import styles from "@/styles/Home.module.scss";
 import Header from "@/components/header";
+import Information from "@/components/Information";
+import Content from "@/components/Content";
 import { NextSeo } from "next-seo";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Header />
+      <Information />
+      <Content />
     </main>
   );
 }
