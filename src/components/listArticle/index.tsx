fc93dc8d79caf84836b8c3850c97ff7508196b48
Vuong Q. Huy <119883477+vnghuygithub@users.noreply.github.com>
2023-04-16 20:26:35 +0700

    commit to homepage

--- src/components/listArticle/index.tsx
+++ src/components/listArticle/index.tsx
@@ -1,51 +1,57 @@
 import React from 'react'
 import styles from "@/styles/Home.module.scss";
+import { Box, Text, Button, space } from '@chakra-ui/react'
+import Image from "next/image";
 
 export default function ListArticle() {
     return (
-        <div className={styles.listArticle}>
-            <div className={styles.listArticle_list}>
-                <img src="/img/listArticle_img1.png" alt=""
+        <Box className={styles.listArticle} >
+            <Box className={styles.listArticle_list}>
+                <Image src="/img/listArticle_img1.png" alt=""
                     className={styles.listArticle_item}
-                    id={styles.listArticle_item_img}
+                    height={159}
+                    width={353}
                 />
-                <p className={styles.listArticle_item} id={styles.author}>By James Landale</p>
-                <p className={styles.listArticle_item} id={styles.team}>TeamliserVCorp</p>
-                <p className={styles.listArticle_item} id={styles.listArticle_item_content}>
+                <Text className={styles.listArticle_item} id={styles.author} paddingRight={28}>By James Landale</Text>
+                <Text className={styles.listArticle_item} id={styles.team} paddingRight={28}>TeamliserVCorp</Text>
+                <Text className={styles.listArticle_item} id={styles.listArticle_item_content} paddingRight={28}>
                     Nghiên cứu cho thấy game thủ có kỹ năng đưa ra quyết
                     định tốt hơn với hoạt động não bộ tăng cường
-                </p>
-                <button className={styles.listArticle_item}
-                    id={styles.listArticle_item_earn}>#EARN</button>
-            </div>
-            <div className={styles.listArticle_list}>
-                <img src="/img/listArticle_img2.png" alt=""
+                </Text>
+                <Button className={styles.listArticle_item}
+                    id={styles.listArticle_item_earn}>#EARN</Button>
+            </Box>
+            <Box className={styles.listArticle_list} marginBottom={16}>
+                <Image src="/img/listArticle_img2.png" alt=""
                     className={styles.listArticle_item}
-                    id={styles.listArticle_item_img}
+                    height={159}
+                    width={376}
                 />
-                <p className={styles.listArticle_item} id={styles.author}>By James Landale</p>
-                <p className={styles.listArticle_item} id={styles.team}>TeamliserVCorp</p>
-                <p className={styles.listArticle_item} id={styles.listArticle_item_content}>
+                <Text className={styles.listArticle_item} id={styles.author} paddingRight={28}>By James Landale</Text>
+                <Text className={styles.listArticle_item} id={styles.team} paddingRight={28}>TeamliserVCorp</Text>
+                <Text className={styles.listArticle_item} id={styles.listArticle_item_content} paddingRight={28}>
                     Dynamic avatars for legacy capture:
                     a revolution of blockchain use in the making
-                </p>
-                <button className={styles.listArticle_item}
-                    id={styles.listArticle_item_earn}>#EARN</button>
-            </div>
-            <div className={styles.listArticle_list}>
-                <img src="/img/listArticle_img3.png" alt=""
+                </Text>
+                <Button className={styles.listArticle_item}
+                    id={styles.listArticle_item_earn}>#EARN</Button>
+            </Box>
+            <Box className={styles.listArticle_list}>
+                <Image src="/img/listArticle_img3.png" alt=""
                     className={styles.listArticle_item}
-                    id={styles.listArticle_item_img}
+                    height={159}
+                    width={376}
+
                 />
-                <p className={styles.listArticle_item} id={styles.author}>By James Landale</p>
-                <p className={styles.listArticle_item} id={styles.team}>TeamliserVCorp</p>
-                <p className={styles.listArticle_item} id={styles.listArticle_item_content}>
+                <Text className={styles.listArticle_item} id={styles.author} paddingRight={28}>By James Landale</Text>
+                <Text className={styles.listArticle_item} id={styles.team} paddingRight={28}>TeamliserVCorp</Text>
+                <Text className={styles.listArticle_item} id={styles.listArticle_item_content} paddingRight={28}>
                     Blockchain.com Shopping Assets to Fill
                     $270M Hole From Three Arrows Capital: Sources
-                </p>
-                <button className={styles.listArticle_item}
-                    id={styles.listArticle_item_earn}>#EARN</button>
-            </div>
-        </div>
+                </Text>
+                <Button className={styles.listArticle_item}
+                    id={styles.listArticle_item_earn}>#EARN</Button>
+            </Box>
+        </Box>
     )
 }
