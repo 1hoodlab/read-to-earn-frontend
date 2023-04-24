224e1c664596a6b7f4495d2a61718ae8f6ab7da9
Vuong Q. Huy <119883477+vnghuygithub@users.noreply.github.com>
2023-04-24 20:56:36 +0700

    feat/homepage_update

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -21,15 +21,14 @@ export default function Login({ isVisible, onClose }: LoginProps) {
                     marginLeft={"auto"} marginRight={"auto"}
                     textAlign={"center"} paddingLeft={67} paddingRight={67} paddingTop={35}>
                     <Text fontWeight={700} fontSize={"30px"} lineHeight={"36px"} >Welcome back!</Text>
-                    <Text fontWeight={500} fontSize={"12px"} lineHeight={"14.52px"} color={"#666666"} marginTop={1}>
+                    <Text fontWeight={500} fontSize={"12px"} lineHeight={"14.52px"} color={"#666666"} marginTop={0.8}>
                         Continue with Google or enter your details.
                     </Text>
-                    <Box border={"2px solid #000000"}
+                    <Flex border={"2px solid #000000"}
                         borderRadius={6}
                         width={352}
                         marginLeft={"auto"}
                         marginRight={"auto"}
-                        display={"flex"}
                         padding={4}
                         justifyContent={"center"}
                         marginTop={34} >
@@ -39,7 +38,7 @@ export default function Login({ isVisible, onClose }: LoginProps) {
                             height={20}
                         />
                         <Text fontWeight={600} fontSize={"14px"} lineHeight={"16.94px"} textAlign={"center"} marginLeft={1.5}>Login with Google</Text>
-                    </Box>
+                    </Flex>
                     <Box display={"flex"} textAlign={"center"} marginLeft={"auto"} marginRight={"auto"} marginTop={34}>
                         <Box height={0.3} background={"#DFDFDF"} width={220} marginTop={1.5} marginLeft={"auto"} ></Box>
                         <Text fontWeight={600} fontSize={"14px"} lineHeight={"16.94px"} width={41}>Or</Text>
@@ -52,12 +51,12 @@ export default function Login({ isVisible, onClose }: LoginProps) {
                     <Box marginTop={10}>
                         <Text textAlign={"left"} fontWeight={500} fontSize={"14px"} lineHeight={"16.94px"} >Password</Text>
                         <Box height={0.5} background={"#DFDFDF"} width={"full"} marginTop={1} ></Box>
-                        <Text textAlign={"right"} fontWeight={600} fontSize={"14px"} lineHeight={"16.94px"} marginTop={1}>Forgot password?</Text>
+                        <Text textAlign={"right"} fontWeight={600} fontSize={"14px"} lineHeight={"16.94px"} marginTop={1.5}>Forgot password?</Text>
                     </Box>
                     <Button width={"full"} height={47} background={"#000000"} color={"#ffffff"} marginTop={50}>Login</Button>
                     <Flex justifyContent={"center"} marginTop={3}>
                         <Text fontWeight={500} fontSize={"14px"} lineHeight={"16.94px"}>Don’t have an account? </Text>
-                        <Text fontWeight={600} fontSize={"14px"} lineHeight={"16.94px"}> Sign up for free </Text>
+                        <Text fontWeight={600} fontSize={"14px"} lineHeight={"16.94px"} marginLeft={1.5}> Sign up for free </Text>
                     </Flex>
                 </Box>
             </Box >
