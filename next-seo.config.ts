94def6086893d9d4945237ed37d9c48fe6eb1be2
hoando <hoando.dev@gmail.com>
2023-04-27 11:24:51 +0700

    refactor: update header

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,26 +1,28 @@
 import { NextSeoProps } from "next-seo";
 
+const DESCRIPTION_DEFAULT =
+  "To attain such decentralized ownership for a new internet architecture, Web 3.0’s proponents argue for a blockchain-oriented structuring of the internet, with the onus of participation and possession relegated to the user at the centre. ";
 export const NEXT_SEO_DEFAULT: NextSeoProps = {
-  titleTemplate: '%s',
-  defaultTitle: 'Snews',
-  description: "News of Web3",
+  titleTemplate: "%s",
+  defaultTitle: "Snews",
+  description: DESCRIPTION_DEFAULT,
   openGraph: {
     type: "website",
     locale: "en_IE",
     title: "Open Graph Snews",
     images: [
-        {
-            url: '',
-            width: 800,
-            height: 600,
-            alt: 'OG Image Alt Snews',
-            type: 'image/jpeg',
-        }
-    ]
+      {
+        url: "/assets/paper.jpeg",
+        width: 800,
+        height: 600,
+        alt: "OG Image Alt Snews",
+        type: "image/jpeg",
+      },
+    ],
   },
   twitter: {
-    handle: '@hoando',
-    site: '@snews',
-    cardType: 'summary_large_image'
+    handle: "@hoando",
+    site: "@snews",
+    cardType: "summary_large_image",
   },
 };
